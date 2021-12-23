291e0b8542e2d2c46d8d7b73ba88dea03bad408c
Mphatso Lichapa <lichapau@gmail.com>
2021-12-23 11:43:29 +0200

    Revert "Add Badge  show a "Reserved" badge on rocket"

--- src/redux/rockets/rocket.js
+++ src/redux/rockets/rocket.js
@@ -1,9 +1,6 @@
 import fetchRockets from '../../components/rockets/Rocketapi';
 
 const GET_ROCKETS = 'rocketStore/rockets/GET_ROCKETS';
-const BOOK_ROCKET = 'rocketStore/rockets/BOOK_ROCKET';
-const CANCLE_ROCKET = 'rocketStore/rockets/CANCLE_ROCKET';
-
 // state
 const initialState = [];
 
@@ -16,27 +13,12 @@ export const getAllRockects = () => async (dispatch) => {
     rockets,
   });
 };
-export const bookRocket = (id) => ({
-  type: BOOK_ROCKET, id,
-});
-export const cancleRocket = (id) => ({
-  type: CANCLE_ROCKET, id,
-});
+
 // reducers
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return action.rockets;
-    case BOOK_ROCKET:
-      return state.map((rocket) => {
-        if (rocket.id !== action.id) return rocket;
-        return { ...rocket, reserved: true };
-      });
-    case CANCLE_ROCKET:
-      return state.map((rocket) => {
-        if (rocket.id !== action.id) return rocket;
-        return { ...rocket, reserved: false };
-      });
     default:
       return state;
   }
