63eeffb84ccc840ea5d108315e5cda012b75b9e5
Lichapa <lichapau@gmail.com>
2021-12-24 04:30:32 +0200

    completed misssions profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import MissionsProfile from './myProfile/MissionsProfile';
+import './myProfile/myProfile.css';
 
-const MyProfile = () => (
-  <div>
-    <h1>My Profile Page </h1>
+const MyProfile = () => {
+  return(
+    <div className="profile-container">
+    <MissionsProfile />    
   </div>
-);
+  )
+
+};
 
 export default MyProfile;
